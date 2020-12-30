15f38ccee6d6b08f64bd836ce6c45131ba039937
Crowdbotics <team@crowdbotics.com>
2020-12-30 06:44:30 +0000

    Update models

--- backend/blusteak_messenger_23606/wsgi.py
+++ backend/blusteak_messenger_23606/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blusteak_messenger_23606.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blusteak_messenger_23606.settings")
 
 application = get_wsgi_application()
